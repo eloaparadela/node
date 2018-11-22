43ecd5d148754e7f5c699b5b826ebcea564bc6e7
Eloá Paradela <eloparadela@gmail.com>
2018-11-21 22:33:20 -0200

    add aulas de node

--- fullstack-aulas-node/node/aula-5/resources.js
+++ fullstack-aulas-node/node/aula-5/resources.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const users = require('./users.js');
+const Joi = require('joi');
 const app = express();
+
 // const Joi = require('joi');
 // const port = process.env.PORT || 3000;
 
@@ -20,8 +22,10 @@ app.get('/api/users/:id', (req, res) => {
 });
 
 app.post('/api/users', (req, res) => {
+  const id = Math.max(...users.map(user => user.id));
+
   const newUser = {
-    id: users.length + 1,
+    id: id,
     name: req.body.name,
     email: req.body.email
   };
@@ -29,6 +33,48 @@ app.post('/api/users', (req, res) => {
   users.push(newUser);
   res.send(newUser);
 });
+
+app.put('/api/users/:id', (req, res) => {
+  const foundUser = users.find(user => user.id === parseInt(req.params.id));
+  const schema ={
+    name: Joi.string().required(),
+    email: Joi.string().required()
+  }
+  const validation = Joi.validate(req.body, schema);
+      
+
+  if(validaton.error){
+    return res.status(400).send(validation.error.details[0].message);
+  }
+
+
+  if (!foundUser){
+    return res.status(404).send('Amigo, cancela!');
+  }
+
+  // if(!req.body.name || !req.body.email){
+  //    return res.status(400).send('Precisa incluir nome e email');
+  // }
+  foundUser.name= req.body.name;
+  foundUser.email =req.body.email;
+  res.send(foundUser);
+});
+
+
+
+ app.delete('/api/users/:id', (req, res) => {
+  const foundUser = users.find(user => user.id === parseInt(req.params.id));
+  if (!foundUser) {
+    return res.status(404).send('Amigo, cancela!');
+  }
+  const index = users.indexOf(foundUser)
+  if( index > -1 ){
+    users.splice(index,1)
+    res.send(foundUser)
+  }
+ });
+
+
 
 // app.put('caminho_para_atualizar_um_usuário', (req, res) => {
 //   // Procure o usuário a partir do id
